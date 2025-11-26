23f67cab36675169583890d538fe5567cea7e71d
Peter Vacho <p_vacho@utb.cz>
2025-11-26 20:14:56 +0100

    Add impl for some basic ml models

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,6 @@
-def main() -> None:
-    """Program entrypoint."""
-    print("Hello world")
+from __future__ import annotations
 
+from src.ml_models import main
 
 if __name__ == "__main__":
     main()
